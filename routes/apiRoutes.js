f88ce79ae618cbea9c782767ccdf45e738084f72
cbarnes0 <carsonbarnes98@gmail.com>
2023-03-16 14:38:10 -0400

    get, post, and delete added after tutor session

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,38 @@
 const api = require('express').Router();
-const db = require('../db/db.json');
+let db = require('../db/db.json');
 const fs = require('fs');
 
 api.get('/notes', function (req, res) {
-    console.log(`${req.method} request received`)
-    fs.readFile('../db/db.json', (err, data) => {
+    console.log(`${req.method} request received`);
+    fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
         dbData = JSON.parse(data);
-        res.send(dbData);
+        res.json(dbData);
     });
+});
+
+api.post('/notes', function (req, res) {
+    console.log(`${req.method} request received`);
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: Math.random()
+    };
+    db.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(db));
+    res.json(db);
+});
+
+api.delete('/notes/:id', function (req, res) {
+    let notesToKeep = [];
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].id != req.params.id) {
+            notesToKeep.push(db[i]);
+        };
+    };
+    db = notesToKeep;
+    fs.writeFileSync("./db/db.json", JSON.stringify(db));
+    res.json(db);
 });
 
 module.exports = api;
